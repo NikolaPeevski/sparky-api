9d069ad831cf6957394845b70219cde3de731a74
nikolapeevski <Jeratull@hotmail.com>
2020-07-29 11:58:41 -0500

    Add Winston logging

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import compression from 'compression'; // compresses requests
 import bodyParser from 'body-parser';
 import lusca from 'lusca';
 import 'reflect-metadata'; // this shim is required
+import cors from 'cors';
+
+import winston from 'winston';
+import expressWinston from 'express-winston';
 
 import {
   createExpressServer,
@@ -57,6 +61,32 @@ app.set('port', process.env.PORT || 3000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+    ),
+    // optional: control whether you want to log the meta data about the request (default to true)
+    meta: true,
+    // optional: customize the default logging message.
+    // E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    // Use the default Express/morgan request formatting.
+    // Enabling this will override any msg if true.Will only output colors with colorize set to true
+    expressFormat: true,
+    // Color the text and status code, using the Express/morgan color palette
+    // (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    colorize: false,
+    // optional: allows to skip some log messages based on request and/or response
+    ignoreRoute(req, res) {
+      return false;
+    },
+  }),
+);
 
 app.use('/', swaggerUiExpress.serve, swaggerUiExpress.setup(spec));
 
